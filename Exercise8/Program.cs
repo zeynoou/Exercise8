b7ac367e046b57e45a44d0f810f86f8ce3ee4c45
Zeynab <Zeynab@ZEYNAB>
2024-03-04 10:46:43 +0330

    Add project files.

---
+++ Exercise8/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise8
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter true or false:");
+            string a = Console.ReadLine();
+            var result1 = Convert.ToBoolean(a);
+            var result2 = Reverse(result1);
+            Console.WriteLine("Reverse of your choise is:" + result2);
+        }
+
+        public static bool Reverse(bool boolean)
+        {
+            return !boolean;
+        }
+    }
+}
